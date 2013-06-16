4e00082ed059943d07072fcfb83513ae56ff9c71
Pēteris Ņikiforovs <p@mak.id.lv>
2013-06-16 22:54:08 +0000

    scanline... work in progress

--- agra.s
+++ agra.s
@@ -1,5 +1,3 @@
-@ todo push
-
 .text
 
 .extern FrameBufferGetAddress
@@ -30,7 +28,7 @@ getPixelAddr:
   cmp r1, r1                @ y
   blt getPixelAddr_oob      @ y < 0
   
-  push {r4,r5,r6}
+  push {r4-r6}
   mov r4, r0                @ x
   mov r5, r1                @ y
   
@@ -55,11 +53,11 @@ getPixelAddr:
   add r1, r1, r4            @ index = width * y + x
   add r0, r0, r1, lsl #2    @ frameBuffer[index]
   
-  pop {r4,r5,r6}
+  pop {r4-r6}
   bx lr
 
 getPixelAddr_oob_pop:
-  pop {r4,r5,r6}
+  pop {r4-r6}
 
 getPixelAddr_oob:
   mov r0, #-1
@@ -105,19 +103,22 @@ line:
   @ r2 = x1
   @ r3 = y1
   
-  push {r4,r5,r6,r7,r8,r10,r12,lr}
+  push {r4-r8,r10,r12,lr}
   
-  mov r6, #1        @ sx
   subs r4, r2, r0   @ dx = x1-x0
   mvnlt r4, r4      @ dx = abs(dx)
   addlt r4, r4, #1  @ dx = abs(dx)
-  movlt r6, #-1     @ sx, if x0 >= x1
+  mov r6, #0        @ sx
+  movgt r6, #1      @ sx
+  movlt r6, #-1     @ sx
   
-  mov r7, #1        @ sy
   subs r5, r3, r1   @ dy = y1-y0
   mvnlt r5, r5      @ dy = abs(dy)
   addlt r5, r5, #1  @ dy = abs(dy)
   movlt r7, #-1     @ sy, if y0 >= y1
+  mov r7, #0        @ sy
+  movgt r7, #1      @ sy
+  movlt r7, #-1     @ sy
   
   sub r8, r4, r5    @ err = dx - dy
   
@@ -149,17 +150,17 @@ line_loop:
   b line_loop
 
 line_plot:
-  push {r0,r1,r2,r3,lr}
+  push {r0-r3,lr}
   mov r2, r10
   bl pixel
-  pop {r0,r1,r2,r3,lr}
+  pop {r0-r3,lr}
   bx lr
   
 line_plot_and_finish:
   bl line_plot
   
 line_done:
-  pop {r4,r5,r6,r7,r8,r10,r12,lr}
+  pop {r4-r8,r10,r12,lr}
   bx lr
 
 triangleFill:
@@ -169,86 +170,149 @@ triangleFill:
   @ r3 = y2
   @ r4 = x3
   @ r5 = y3
-
-  @ line (x1,y1) => (x2,y2)
-  push {r0,r1,r2,r3,lr}
-  bl line
-  pop {r0,r1,r2,r3,lr}
   
-  ldr r4, [sp]      @ x3
-  ldr r5, [sp, #4]  @ y3
+  push {r4-r12}
   
-  @ line (x1,y1) => (x3,y3)
-  push {r0,r1,r2,r3,r4,r5,lr}
-  mov r2, r4
-  mov r3, r5
-  bl line
-  pop {r0,r1,r2,r3,r4,r5,lr}
+  subs r4, r2, r0   @ dx1 = x2-x1
+  mvnlt r4, r4      @ dx1 = abs(dx2)
+  addlt r4, r4, #1  @ dx1 = abs(dx2)
+  mov r6, #0        @ sx1
+  movgt r6, #1      @ sx1
+  movlt r6, #-1     @ sx1
   
-  @ line (x3,y3) => (x2,y2)
-  push {r0,r1,r2,r3,r4,r5,lr}
-  mov r0, r4
-  mov r1, r5
-  bl line
-  pop {r0,r1,r2,r3,r4,r5,lr}
+  mov r12, r4       @ i = dx1
   
-  @ triangle center
-  add r6, r0, r2          @ x1 + x2
-  add r6, r6, r4          @ x1 + x2 + x3
-  add r7, r1, r3          @ y1 + y2
-  add r7, r7, r5          @ y1 + y2 + y3
-  ldr r8, =0x55555556
-  umull r10, r0, r6, r8   @ r0 = (x1+x2+x3)/3
-  umull r10, r1, r7, r8   @ r1 = (y1+y2+y3)/3
+  subs r5, r3, r1   @ dy1 = y2-y1
+  mvnlt r5, r5      @ dy1 = abs(dy1)
+  addlt r5, r5, #1  @ dy1 = abs(dy1)
+  mov r7, #0        @ sy1
+  movgt r7, #1      @ sy1
+  movlt r7, #-1     @ sy1
   
-  @ flood fill from the center
-  push {lr}
-  ldr r2, currentColor_word
-  bl floodfill
-  pop {lr}
+  add r8, r5, r5    @ e1 = 2 * dy1
+  sub r8, r8, r4    @ e1 = 2 * dy1 - dx1
   
-  bx lr
-
-floodfill:
-  @ r0 = x
-  @ r1 = y
-  @ r2 = target color
-  push {r0,r1,r2,lr}
-  bl getPixelAddr     @ r0 = frameBuffer[index]
-  mov r3, r0          @ frameBuffer[index] 
-  pop {r0,r1,r2,lr}
-  cmp r3, #-1
-  bxeq lr             @ out of bounds
-  
-  @ TODO: kas ir, ja operācija?
-  ldr r4, [r2]        @ target color
-  ldr r5, [r3]        @ node color
+  @ if dy1 > dx1 then swap(dx1, dy1)
   cmp r5, r4
-  bxeq lr
+  movgt r10, r4
+  movgt r4, r5
+  movgt r5, r10
+  movgt r10, #1
+  movle r10, #0
   
-  push {r0,r1,r2,lr}
-  bl pixel
-  pop {r0,r1,r2}
+  ldr r2, [sp, #36]  @ x3
+  ldr r3, [sp, #40]  @ y3
   
-  push {r0,r1}
-  add r1, r1, #1  @ x, y+1
-  bl floodfill
-  pop {r0,r1}
+  push {r4-r8,r10}  @ dx1, dy1, sx1, sy1, e1, changed1
   
-  push {r0,r1}
-  add r0, r0, #1  @ x+1, y
-  bl floodfill
-  pop {r0,r1}
+  subs r4, r2, r0   @ dx2 = x3-x1
+  mvnlt r4, r4      @ dx2 = abs(dx2)
+  addlt r4, r4, #1  @ dx2 = abs(dx2)
+  mov r6, #0        @ sx2
+  movgt r6, #1      @ sx2
+  movlt r6, #-1     @ sx2
   
-  push {r0,r1}
-  sub r0, r0, #1  @ x-1, y
-  bl floodfill
-  pop {r0,r1}
+  subs r5, r3, r1   @ dy2 = y3-y1
+  mvnlt r5, r5      @ dy2 = abs(dy2)
+  addlt r5, r5, #1  @ dy2 = abs(dy2)
+  mov r7, #0        @ sy2
+  movgt r7, #1      @ sy2
+  movlt r7, #-1     @ sy2
   
-  sub r1, r1, #1  @ x, y-1
-  bl floodfill
+  add r8, r5, r5    @ e2 = 2 * dy2
+  sub r8, r8, r4    @ e2 = 2 * dy2 - dx2
   
-  pop {lr}
+  @ if dy2 > dx2 then swap(dx2, dy2)
+  cmp r5, r4
+  movgt r10, r4
+  movgt r4, r5
+  movgt r5, r10
+  movgt r10, #1
+  movle r10, #0
+  
+  push {r4-r8,r10}      @ dx2, dy2, sx2, sy2, e2, changed2
+  
+  mov r2, r0
+  mov r3, r1
+  
+triangleFill_loop:
+  push {r0-r3,lr}
+  bl line
+  pop {r0-r3,lr}
+  
+  @ 0 4 8 12 16 20       24 28 32 36 40 44
+  ldr r4, [sp, #40]  @ e1   
+  ldr r5, [sp, #44]  @ changed1
+  cmp r5, #1
+  ldreq r6, [sp, #32]  @ sx1
+  ldrne r6, [sp, #36]  @ sy1
+  ldr r7, [sp, #24]    @ dx1
+  ldr r8, [sp, #28]    @ dy1
+  
+  cmp r4, #0
+  blt triangleFill_while_e1_done
+
+triangleFill_while_e1:
+  cmp r5, #1
+  addeq r0, r0, r6   @ x1 += sx1
+  addne r1, r1, r6   @ y1 += sy1
+  sub r4, r4, r7     @ e1 -= dx1
+  subs r4, r4, r7    @ e1 -= 2*dx1
+  bge triangleFill_while_e1
+
+triangleFill_while_e1_done:
+  cmp r5, #1
+  addeq r1, r1, r6
+  addne r0, r0, r6
+  
+  add r4, r4, r8     @ e1 += dy1
+  add r4, r4, r8     @ e1 += 2*dy1
+  str r4, [sp, #40]
+
+  cmp r3, r1
+  beq triangleFill_while_yy_done
+
+  ldr r4, [sp, #16]   @ e2
+  ldr r5, [sp, #20]   @ changed2
+  cmp r4, #0
+  ldreq r6, [sp, #8]  @ sx2
+  ldrne r6, [sp, #12] @ sy2
+  ldr r7, [sp, #0]    @ dx2
+  ldr r8, [sp, #4]    @ dy2
+
+triangleFill_while_yy:
+  cmp r4, #0
+  blt triangleFill_while_e2_done
+
+triangleFill_while_e2:
+  cmp r5, #1
+  addeq r2, r2, r6   @ x2 += sx2
+  addne r3, r3, r6   @ y2 += sy2
+  sub r4, r4, r7     @ e2 -= dx2
+  subs r4, r4, r7    @ e2 -= 2*dx2
+  bge triangleFill_while_e2
+
+triangleFill_while_e2_done:
+  cmp r5, #1
+  addeq r3, r3, r6
+  addne r2, r2, r6
+  
+  add r4, r4, r8     @ e2 += dy2
+  add r4, r4, r8     @ e2 += 2*dy2
+  str r4, [sp, #40]
+  
+  cmp r3, r1
+  bne triangleFill_while_yy
+
+triangleFill_while_yy_done:  
+  subs r12, r12, #1       @ i--
+  bge triangleFill_loop   @ i >= 0
+  
+triangleFill_done:  
+  pop {r4-r8,r10}
+  pop {r4-r8,r10}
+  
+  pop {r4-r12}
   bx lr
 
 circle:
@@ -259,7 +323,7 @@ circle:
   cmp r2, #0
   bxlt lr         @ radius < 0
   
-  push {r4,r5,r6,r7,r8,r10}
+  push {r4-r8,r10}
   
   mov r4, r2      @ x = radius
   mov r5, #0      @ y = 0
@@ -326,11 +390,11 @@ circle_loop:
   cmp r4, r5        @ x, y
   bge circle_loop   @ x >= y
 
-  pop {r4,r5,r6,r7,r8,r10}
+  pop {r4-r7,r8,r10}
   bx lr
 
 .data
-format: .asciz "%x\n"
+format: .asciz "%d\n"
 format_a: .word format
 
 
--- agra_main.c
+++ agra_main.c
@@ -14,7 +14,7 @@ pixcolor_t *make_color(unsigned r, unsigned g, unsigned b, unsigned op) {
 
 int main() {
     pixcolor_t *black = make_color(0, 0, 0, 0);
-    pixcolor_t *white = make_color(COLOR_DEPTH, COLOR_DEPTH, COLOR_DEPTH, 0);
+    pixcolor_t *white = make_color(0x3ff, 0x3ff, 0x3ff, 0);
     pixcolor_t *red = make_color(0x3ff, 0, 0, 0);
     pixcolor_t *green = make_color(0, 0x3ff, 0, 0);
     pixcolor_t *blue = make_color(0, 0, 0x3ff, 0);
@@ -31,17 +31,30 @@ int main() {
     pixel(25, 2, white);
     
     setPixColor(blue);
-    line(0, 0, 39, 19);
+    //line(0, 0, 39, 19);
     
     setPixColor(green);
     //triangleFill(0, 19, 20, 19, 0, 10);
+    triangleFill(15, 0, 3, 10, 37, 10);
     
     setPixColor(red);
-    circle(20, 10, 5);
+    //circle(20, 10, 5);
     
+    pixcolor_t *white_and = make_color(0x3ff, 0x3ff, 0x3ff, 1);
     pixcolor_t *blue_or = make_color(0, 0, 0x3ff, 2);
+    pixcolor_t *green_xor = make_color(0, 0x3ff, 0, 3);
+    
+    // op = and
+    pixel(37, 0, red); // sarkans = R
+    pixel(37, 0, white_and); // joprojām sarkans = R
+    
+    // op = or
+    pixel(38, 0, red); // sarkans = R
+    pixel(38, 0, blue_or); // sarkanzils = M
+    
+    // op = xor
     pixel(39, 0, red); // sarkans = R
-    pixel(39, 0, blue_or); // sarkan zils = M
+    pixel(39, 0, green_xor); // sarkanzaļš = Y
     
     FrameShow();
     return 0;
